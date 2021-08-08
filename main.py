46c9b59d01360088aeec600b2aeea4aad961e5ed
Kamiklza <kamiklza@hotmail.com>
2021-08-08 19:59:51 +0800

    added grid position and padding

--- main.py
+++ main.py
@@ -7,21 +7,30 @@ window.minsize(width=500, height=300)
 
 #Lable
 my_label = tkinter.Label(text="Welcome")
-my_label.pack()
+my_label.grid(column=0, row=0)
+my_label.config(padx=20, pady=20)
 
 
 def change_text():
-    my_label["text"] = input.get()
-    
+    my_label["text"] = input_text.get()
+
 
 #Button
 button = tkinter.Button(text="Click", command=change_text)
-button.pack()
+button.grid(column=1, row=1)
+button.config(padx=20, pady=20)
 
 
 #Entry
-input = tkinter.Entry(width=10)
-input.pack()
+input_text = tkinter.Entry(width=10)
+input_text.grid(column=3, row=2)
+input_text.grid(padx=10, pady=10)
+
+
+
+#New Button
+new_button = tkinter.Button(text="Click")
+new_button.grid(column=2, row=0)
 
 
 
